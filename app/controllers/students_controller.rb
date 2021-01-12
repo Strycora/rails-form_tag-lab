73142efa593b9a96621e03cb9b1c0af793c5b019
Vikram Chatterjee <strycora@gmail.com>
2021-01-12 16:38:32 -0700

    commit tree ccb36a4592d793285ee6da3e6cb066a6e21ee87b

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,10 +1,24 @@
 class StudentsController < ApplicationController
-  
+  before_action :select_student, only: :show
+
   def index
     @students = Student.all
   end
 
   def show
+  end
+
+  def new
+  end
+
+  def create
+    session[:form_params] = params.inspect
+    redirect_to new_student_path
+  end
+
+  private
+
+  def select_student
     @student = Student.find(params[:id])
   end
 
